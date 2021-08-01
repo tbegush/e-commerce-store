bbb6e27fd85c67ecde663cd5a5b164684597793f
thomas.begush <tbegush@gmail.com>
2021-08-01 18:50:43 -0400

    including models

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,16 +8,9 @@ router.get("/", (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-    attributes: [
-      "id",
-      "category_id"
-    ],
-    include: [
+     include: [
       {
-        model: Product,
-        attributes: [
-          "id", "product_name", "price", "stock", "category_id"
-        ]
+        model: Category,
       },
       {
         model: Tag
